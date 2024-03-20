c9b064f2c1f4ef7bc6860bdaba6eec36e158bb79
SRJ <sarojkumardey.contact@gmail.com>
2024-03-20 17:32:21 +0530

    media query changed for about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,12 +3,12 @@ import img from "../assets/setup.jpg";
 
 function AboutMe() {
   return (
-    <div className="bg-black2 mt-32 flex justify-between  flex-col md:flex-row items-center relative p-10">
+    <div className="bg-black2 mt-32 flex justify-between  flex-col lg:flex-row items-center relative p-10">
       <div className=" -top-20  h-h2 w-w2 md:h-h1 md:w-w1 overflow-hidden flex-shrink-0 rounded  absolute ">
         <img src={img} alt="image" className=" h-full w-full object-cover" />
       </div>
       <div className=" h-h2x w-w2 md:h-h1x md:w-w1 "></div>
-      <div className="h-h2x md:h-h1x w-full md:w-1/2">
+      <div className="h-h2x lg: md:h-h1x w-full md:w-1/2">
        <div className="flex flex-col">
        <span className="pb-6">About Us</span>
         <span className="pb-6">Save your time by using Trydo and present yours.</span>
