a8417bf427f2aef009a70466f72eb9ed71c469c1
SRJ <sarojkumardey.contact@gmail.com>
2024-03-21 16:01:41 +0530

    MAIL

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,6 @@
 import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
-  const redirectToGmail = () => {
-    const email = "sarojkumardey.mail@gmail.com";
-    const url = "https://mail.google.com/mail/?view=cm&to=" + email;
-    window.location.href = url;
-  };
   return (
     <header className="flex items-center justify-between ">
       <h1 className=" ml-4 mt-3 cursor-pointer font-font1 text-5xl font-bold md:ml-10 md:mt-6   ">
@@ -23,12 +18,14 @@ function Navbar() {
           <li className="mx-6 hidden cursor-pointer transition-all  duration-300 ease-in-out hover:text-blue2 md:block">
             Blog
           </li>
-          <li
-            onClick={redirectToGmail}
+          <a
+            href="mailto: sarojkumardey.mail@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
             className="mx-6  flex cursor-pointer items-center justify-center rounded-md border-2 border-solid border-white bg-black bg-opacity-10 px-5 py-2 transition-all  duration-300 ease-in-out hover:-translate-y-0.5 hover:border-blue2 hover:bg-blue2 "
           >
             LET'S TALK
-          </li>
+          </a>
           <li className="mx-2  cursor-pointer text-4xl md:hidden">
             <IoMenu />
           </li>
